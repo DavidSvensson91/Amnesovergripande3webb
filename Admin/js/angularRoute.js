3b91db9d79dfbd42755acac49e7fec87e9d26fc8
JoakimRW <rtuu41@gmail.com>
2017-02-22 14:17:44 +0100

    Cleaned some Code

--- Admin/js/angularRoute.js
+++ Admin/js/angularRoute.js
@@ -3,15 +3,9 @@ var app = angular.module('myApp', ['ngRoute']);
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/',
-            { templateUrl: 'partials/frontpage.html' })
+            {templateUrl: 'partials/frontpage.html'})
         .when('/vaderdata',
-            { templateUrl: 'partials/vaderdata.html' })
-        .when('/addstation', 
-            { templateUrl: "partials/addStation.html" })
-        .when('/deletedata', 
-            { templateUrl: "partials/deleteData.html" })
-        .when('/login',
-            { templateUrl: "partials/Nmhi-login.html" })
-        .when('/searchweather',
-            { templateUrl: "partials/searchWeather.html" });
+            {templateUrl: 'partials/vaderdata.html'})
+        .when('/deletedata',
+            {templateUrl: "partials/deleteData.html"})
 });
